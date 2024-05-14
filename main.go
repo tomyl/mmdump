8bdc432953c3b87cb6de0a4893e9a253826cb6ca
Tommy Lindgren <tommy.lindgren@zebware.com>
2024-05-14 14:08:47 +0200

    Rudimentary index search

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"text/tabwriter"
 	"time"
+
+	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/search/highlight/highlighter/ansi"
 )
 
 type user struct {
@@ -47,6 +51,11 @@ type channel struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	DisplayName string `json:"display_name"`
+}
+
+type message struct {
+	From string
+	Body string
 }
 
 func get(endpoint, cookie, resource string, ignore404 bool) ([]byte, error) {
@@ -130,6 +139,7 @@ func getCached(endpoint, cookie, resource, dir, base string, ignore404 bool) ([]
 
 func dumpPosts(endpoint, cookie, dir, channelID string) error {
 	before := ""
+	after := ""
 	for {
 		resource := fmt.Sprintf("channels/%s/posts?before=%s&per_page=1000", channelID, before)
 		name := fmt.Sprintf("posts/%s_%s.json", channelID, before)
@@ -161,7 +171,15 @@ func dumpPosts(endpoint, cookie, dir, channelID string) error {
 			break
 		}
 
+		if before == "" {
+			after = posts.Order[0]
+		}
+
 		before = posts.Order[len(posts.Order)-1]
+	}
+
+	if after != "" {
+		log.Printf("qqq %q", after)
 	}
 
 	return nil
@@ -276,6 +294,10 @@ func listPostsBefore(dir, channelID, before string, userMap map[string]string, w
 		fmt.Fprintf(w, "%s\t%s\t%s\n", t, u, p.Message)
 	}
 
+	if before == "" {
+		// TODO: after
+	}
+
 	return nil
 }
 
@@ -308,10 +330,144 @@ func listPosts(dir, channelID string) error {
 	return nil
 }
 
+func index(dir string) error {
+	name := path.Join(dir, "index.bleve")
+	if _, err := os.Stat(name); err == nil || !errors.Is(err, os.ErrNotExist) {
+		if err == nil {
+			log.Printf("Found index %s", name)
+			return nil
+		}
+		return err
+	}
+	log.Printf("Creating index %s", name)
+	mapping := bleve.NewIndexMapping()
+	index, err := bleve.New(name, mapping)
+	if err != nil {
+		return err
+	}
+	return indexChannels(dir, index)
+}
+
+func indexChannels(dir string, index bleve.Index) error {
+	name := filepath.Join(dir, "channels.json")
+	channelsBody, err := os.ReadFile(name)
+	if err != nil {
+		return err
+	}
+
+	var channels []channel
+	if err := json.Unmarshal(channelsBody, &channels); err != nil {
+		return err
+	}
+
+	for _, c := range channels {
+		if err := indexChannel(dir, index, c); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func indexChannel(dir string, index bleve.Index, c channel) error {
+	log.Printf("Indexing %s %s", c.ID, c.DisplayName)
+	name := filepath.Join(dir, "users.json")
+	usersBody, err := os.ReadFile(name)
+	if err != nil {
+		return err
+	}
+
+	var users []user
+	if err := json.Unmarshal(usersBody, &users); err != nil {
+		return err
+	}
+
+	userMap := make(map[string]string, len(users))
+	for _, u := range users {
+		userMap[u.ID] = u.Username
+	}
+
+	batch := index.NewBatch()
+
+	if err := indexPostsBefore(dir, batch, c, "", userMap); err != nil {
+		return err
+	}
+
+	return index.Batch(batch)
+}
+
+func indexPostsBefore(dir string, batch *bleve.Batch, c channel, before string, userMap map[string]string) error {
+	name := filepath.Join(dir, "posts", fmt.Sprintf("%s_%s.json", c.ID, before))
+	postsBody, err := os.ReadFile(name)
+	if err != nil {
+		return err
+	}
+
+	var posts postsEnvelope
+	if err := json.Unmarshal(postsBody, &posts); err != nil {
+		return err
+	}
+
+	if len(posts.Order) > 0 {
+		indexPostsBefore(dir, batch, c, posts.Order[len(posts.Order)-1], userMap)
+	}
+
+	for i := range posts.Order {
+		postID := posts.Order[len(posts.Order)-i-1]
+		p := posts.Posts[postID]
+		u := userMap[p.UserID]
+		if err := batch.Index(postID, message{
+			From: u,
+			Body: p.Message,
+		}); err != nil {
+			return err
+		}
+	}
+
+	if before == "" {
+		// TODO: after
+	}
+
+	return nil
+}
+
+func query(dir, q string) error {
+	name := path.Join(dir, "index.bleve")
+	index, err := bleve.Open(name)
+	if err != nil {
+		return err
+	}
+
+	query := bleve.NewMatchQuery(q)
+	search := bleve.NewSearchRequest(query)
+	search.Highlight = bleve.NewHighlightWithStyle(ansi.Name)
+	searchResults, err := index.Search(search)
+	if err != nil {
+		return err
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+	fmt.Fprintf(w, "ID\tMessage\n")
+
+	for _, hit := range searchResults.Hits {
+		fmt.Fprintf(w, "%s\t", hit.ID)
+		for i := range hit.Fragments["Body"] {
+			fmt.Fprint(w, hit.Fragments["Body"][i])
+		}
+		fmt.Fprint(w, "\n")
+	}
+
+	w.Flush()
+
+	fmt.Println(searchResults)
+	return nil
+}
+
 func run() error {
 	doDump := flag.Bool("dump", false, "Dump data from Mattermost")
 	doListChannels := flag.Bool("channels", false, "List channels")
 	doListPosts := flag.String("posts", "", "List posts for provided channel ID")
+	doQueryPosts := flag.String("query", "", "Query posts")
 
 	endpoint := flag.String("endpoint", "", "The API endpoint e.g. https://mattermost.example.com/api/v4/")
 	cookie := flag.String("cookie", "", "Mattermost cookie")
@@ -350,6 +506,14 @@ func run() error {
 			return errors.New("-dir not provided")
 		}
 		return listPosts(*dir, *doListPosts)
+	} else if *doQueryPosts != "" {
+		if *dir == "" {
+			return errors.New("-dir not provided")
+		}
+		if err := index(*dir); err != nil {
+			return err
+		}
+		return query(*dir, *doQueryPosts)
 	}
 
 	return errors.New("provide -dump, -channels or -posts")
